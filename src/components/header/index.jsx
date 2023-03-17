d6fa2366dad7faa49c7c4ef2ed7bf57236044b5c
Bruno <brunoalmeida0805@gmail.com>
2023-03-16 22:18:44 -0300

    Primeira parte do projeto finalizado

---
+++ src/components/header/index.jsx
@@ -0,0 +1,25 @@
+import { RiShutDownLine } from "react-icons/ri";
+
+import { Container, Profile, Logout } from "./style";
+
+export function Header() {
+  return (
+    <Container>
+      <Profile>
+        <img
+          src="https://github.com/bruno-deoliveira.png"
+          alt="foto do usuário"
+        />
+
+        <div>
+          <span> Bem-vindo </span>
+          <strong> Bruno Oliveira </strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
+    </Container>
+  );
+}
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,57 @@
+import {Container, Links, Content} from "./style";
+
+import { Tag } from "../../components/tags";
+import { Button } from "../../components/button";
+import { Header } from "../../components/header";
+import { Section } from "../../components/section";
+import { ButtonText } from "../../components/buttonText";
+
+
+
+
+export function Details(){
+  
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1>Introdução ao React</h1>
+
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="Express" />
+            <Tag title="Nodejs" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
+    </Container>
+  );
+}
